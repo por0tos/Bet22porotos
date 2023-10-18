537756fe040b6712e27fed160d02e720857aced4
xiiomara19 <xiomaracaceres19.07@gmail.com>
2023-10-19 00:04:05 +0200

    code smell 11

--- src/main/java/dataAccess/DataAccess.java
+++ src/main/java/dataAccess/DataAccess.java
@@ -691,7 +691,7 @@ public void open(boolean initializeMode){
 			  emf = Persistence.createEntityManagerFactory("objectdb:"+fileName);
 			  db = emf.createEntityManager();
 		} else {
-			Map<String, String> properties = new HashMap<String, String>();
+			Map<String, String> properties = new HashMap<>();
 			  properties.put("javax.persistence.jdbc.user", c.getUser());
 			  properties.put("javax.persistence.jdbc.password", c.getPassword());
 
